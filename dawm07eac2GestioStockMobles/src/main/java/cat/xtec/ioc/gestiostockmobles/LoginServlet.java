c30b26706e47bb663a7766d19ebbf96e1a6a0495
Victor Miguel Forner <vicforner@gmail.com>
2024-10-29 21:44:21 +0100

    TODO Ex1 fet

--- dawm07eac2GestioStockMobles/src/main/java/cat/xtec/ioc/gestiostockmobles/LoginServlet.java
+++ dawm07eac2GestioStockMobles/src/main/java/cat/xtec/ioc/gestiostockmobles/LoginServlet.java
@@ -15,10 +15,20 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String usuari = request.getParameter("nomUsuari");
+        String contrasenya = request.getParameter("contrasenya");
         
         // TODO EX1 - Validació simple per a exemple (usuari: el teu nom, contrasenya: el teu cognom)
         // TODO EX1 - En cas de no entrar quedar-nos a login.html
+        
         // TODO EX1 - L'usuari ha de mantenir-se durant tota la sessió
+        if(usuari.equals("Victor") && contrasenya.equals("Miguel")){
+                HttpSession session = request.getSession();
+                session.setAttribute("usuari", usuari);
+                response.sendRedirect(request.getContextPath() + "/AltaMoble.html");
+            }else{
+            response.sendRedirect(request.getContextPath() + "/Login.html");
+        }
         
     }
 }

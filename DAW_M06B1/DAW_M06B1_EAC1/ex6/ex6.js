ea4d6f18247d98b502c755010cc1eea87ec09cdb
Durwian <vicforner@gmail.com>
2025-03-14 23:54:48 +0100

    Ex 1 Ex 2

---
+++ DAW_M06B1/DAW_M06B1_EAC1/ex6/ex6.js
@@ -0,0 +1,15 @@
+// Funció comprar
+function comprar(refresc) {
+    alert("Aquí té la seva llauna de " + refresc + ".");
+    
+    // Escriu aquí el teu codi
+}
+
+
+// Funció mostrar log
+function log() {
+    console.log("Històric de vendes:");
+    // Escriu aquí el teu codi
+
+
+}
